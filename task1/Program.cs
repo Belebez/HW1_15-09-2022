c158e412212ebe10d05f1c005f3189f6bf17dc96
Viktor <viktor.belebezyev@yandex.ru>
2022-09-15 22:51:19 +0300

    add solution algorithm

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,21 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Программа для определения большего числа");
+
+//ввод значений
+
+Console.Write("Введите первое число - ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int number2 = int.Parse(Console.ReadLine());
+
+//сравним числа, сделаем вывод большего и меньшего числа
+
+if (number1 < number2)
+{
+   Console.WriteLine($"{number2} большее число, {number1} меньшее."); 
+}
+else
+{
+    Console.WriteLine($"{number1} больше число, {number2} меньшее.");
+}
